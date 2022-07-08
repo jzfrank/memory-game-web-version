6e56ba3658104aa796766b860a9bda02be580cec
jzfrank <frankjinzhang@gmail.com>
2022-07-08 16:58:41 +0200

    responsive design

--- src/components/MainWindow.js
+++ src/components/MainWindow.js
@@ -12,26 +12,30 @@ const MainWindow = () => {
   const title = <h1>Superhero Memory Game</h1>;
   return (
     <Fragment>
-      <div className="row container">
-        <div className="col s10">{title}</div>
-        <div className="col s2">
+      {/* <div className="row container">
+        <div className="col s8">{title}</div>
+        <div className="col s4">
           <ScoreBoard pairsClicked={pairsClicked} pairsGuessed={pairsGuessed} />
         </div>
+      </div> */}
+      <div className="container">
+        {title}
+        <ScoreBoard pairsClicked={pairsClicked} pairsGuessed={pairsGuessed} />
+        {pairsGuessed !== totalPairs && (
+          <GameBoard
+            incrementPairsClicked={() => setPairsClicked((prev) => prev + 1)}
+            incrementPairsGuessed={() => setPairsGuessed((prev) => prev + 1)}
+          />
+        )}
+        {pairsGuessed === totalPairs && (
+          <>
+            <h1>Mission Accomplished!</h1>
+            <h1>You used {pairsClicked} trials to make all cards vanish.</h1>
+            <h1>Great job!</h1>
+            <h2>Refresh to restart</h2>
+          </>
+        )}
       </div>
-      {pairsGuessed !== totalPairs && (
-        <GameBoard
-          incrementPairsClicked={() => setPairsClicked((prev) => prev + 1)}
-          incrementPairsGuessed={() => setPairsGuessed((prev) => prev + 1)}
-        />
-      )}
-      {pairsGuessed === totalPairs && (
-        <>
-          <h1>Mission Accomplished!</h1>
-          <h1>You used {pairsClicked} trials to make all cards vanish.</h1>
-          <h1>Great job!</h1>
-          <h2>Refresh to restart</h2>
-        </>
-      )}
     </Fragment>
   );
 };
